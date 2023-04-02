0ac9edbf61f9eaa7fdeedf3a402e7457320da52b
InvalidArgument3 <51190031+InvalidArgument3@users.noreply.github.com>
2023-04-02 02:35:16 -0500

    diagonal zone placement. also gitignore

--- Data/Scripts/ShipPoints/PointCheck.cs
+++ Data/Scripts/ShipPoints/PointCheck.cs
@@ -63,8 +63,8 @@ namespace klime.PointCheck
         public static RtsApi RTS_api { get; private set; }
 
         public Vector3 ctrpoint = new Vector3(0, 0, 0);
-        public Vector3 ctrpoint2 = new Vector3(0, 0, 9000);
-        public Vector3 ctrpoint3 = new Vector3(0, 0, -9000);
+        public Vector3 ctrpoint2 = new Vector3(4500, 0, 9000);
+        public Vector3 ctrpoint3 = new Vector3(-4500, 0, -9000);
         public double capdist = 1000;
         public double capdistCenter = 1000;
         public Vector3 capdistScale;
@@ -2541,8 +2541,8 @@ internal void UpdateCapZone3()
                 case 3:
                     CapOut = 20;
                     ctrpoint = new Vector3(0, 0, 0);
-                    ctrpoint2 = new Vector3(0, 0, 9000);
-                    ctrpoint3 = new Vector3(0, 0, -9000);
+                    ctrpoint2 = new Vector3(4500, 0, 9000);
+                    ctrpoint3 = new Vector3(-4500, 0, -9000);
                     capdist = 1000;
                     capdistCenter = 1000;
                     //MyAPIGateway.Utilities.ShowMessage("GM" , "capdist set to " + capdist);
